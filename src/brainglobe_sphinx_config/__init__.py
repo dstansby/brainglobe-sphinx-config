a02a176be373bdc64592d89c88f3156b2e292246
David Stansby <dstansby@gmail.com>
2023-03-29 14:06:21 +0100

    Add basic header structure

--- src/brainglobe_sphinx_config/__init__.py
+++ src/brainglobe_sphinx_config/__init__.py
@@ -2,10 +2,8 @@ from pathlib import Path
 
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-  "external_links": [
-      {"name": "About", "url": "https://brainglobe.info/about"},
-  ],
   "navbar_start": ["navbar-logo", "version"],
+  "navbar_center": ["header"],
 }
 
 html_logo = "https://brainglobe.info/images/brainglobe.png"
