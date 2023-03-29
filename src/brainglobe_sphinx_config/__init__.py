b1159693dfd170781d37abda2108a9aa0ed81720
David Stansby <dstansby@gmail.com>
2023-03-29 14:25:36 +0100

    Start header links

--- src/brainglobe_sphinx_config/__init__.py
+++ src/brainglobe_sphinx_config/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import json
 
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
@@ -7,4 +8,11 @@ html_theme_options = {
 }
 
 html_logo = "https://brainglobe.info/images/brainglobe.png"
-templates_path = [str(Path(__file__).parent / '_templates')]
+_templates_Path = Path(__file__).parent / '_templates'
+templates_path = [str(_templates_Path)]
+
+header_links = json.load(open(_templates_Path / 'header_links.json'))
+
+html_context = {
+    'header_links': header_links
+}
