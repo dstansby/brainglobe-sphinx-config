eeb920366cb5609b9d3dc3a9674401c8af9ad354
David Stansby <dstansby@gmail.com>
2023-03-29 13:49:43 +0100

    Add logo

--- src/brainglobe_sphinx_config/__init__.py
+++ src/brainglobe_sphinx_config/__init__.py
@@ -1,2 +1,7 @@
-
 html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+  "external_links": [
+      {"name": "About", "url": "https://brainglobe.info/about"},
+  ]
+}
+html_logo = "https://brainglobe.info/images/brainglobe.png"
